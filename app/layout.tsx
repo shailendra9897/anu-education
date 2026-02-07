24cc07db0a1ef5e150f80d62d669d98074da17bf
anu education <anueducation@anus-MacBook-Air.local>
2026-02-07 14:19:55 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics 4 */}
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WBZTN4ZV');
+          `}
+        </Script>
+
+        {/* Google Analytics 4 (KEEP THIS – NOT DELETED) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-YQWGP9339T"
           strategy="afterInteractive"
@@ -35,7 +49,17 @@ export default function RootLayout({
       </head>
 
       <body className="bg-white text-gray-900">
-        <ClientWrapper /> {/* Contains LeadModal + Header */}
+        {/* GTM NoScript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WBZTN4ZV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        <ClientWrapper />
         <main className="min-h-screen">{children}</main>
         <Footer />
       </body>
