de4604e6972e64bc894166dcd1a1d07d23f89b38
anu education <anueducation@anus-MacBook-Air.local>
2026-02-07 14:59:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,21 +17,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
-<Script id="gtm-script" strategy="beforeInteractive">
-  {`
-    (function(w,d,s,l,i){w[l]=w[l]||[];
-    w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-    var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-    j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-    f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-WBZTN4ZV');
-  `}
-</Script>
+        {/* ✅ GOOGLE TAG MANAGER */}
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WBZTN4ZV');
+            `,
+          }}
+        />
 
-        {/* Google Analytics 4 (KEEP THIS – NOT DELETED) */}
+        {/* ✅ GOOGLE ANALYTICS 4 (KEEP THIS) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-YQWGP9339T"
           strategy="afterInteractive"
@@ -49,7 +53,7 @@ export default function RootLayout({
       </head>
 
       <body className="bg-white text-gray-900">
-        {/* GTM NoScript */}
+        {/* ✅ GTM NOSCRIPT (REQUIRED) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-WBZTN4ZV"
